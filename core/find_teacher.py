4abe019c2e38ed5088a62c5404fd8bd4a9f8f059
wywdelpy <e4orez@gmail.com>
2022-09-03 14:06:11 +0300

    upgrade find_teacher.py

--- core/find_teacher.py
+++ core/find_teacher.py
@@ -3,9 +3,10 @@ def find_teacher(name):
     import fake_useragent
     from bs4 import BeautifulSoup
 
-    name = name.replace('.',' ')
-    name = name.split()
-    # print(name)
+    if '.' in name:
+        name = name.replace('.',' ')
+        name = name.split()
+        # print(name)
 
     with open('all_teachers.txt', 'r') as file:
         all_groups = file.readlines()
@@ -13,15 +14,28 @@ def find_teacher(name):
         line = line.replace('\n','')
         line_ziped = line.split(' : ')
         line_ziped = line_ziped[0].split()
-        if name[0].upper() in line_ziped[0].upper():
-            if name[1].upper() in line_ziped[1].upper():
-                if name[2].upper() in line_ziped[2].upper():
-                    line = line.split(' : ')
-                    text = [
-                    f'<b>Имя</b>: {line[0]}',
-                    f'<b>Ссылка</b>: {line[1]}'
-                    ]
-                    text = '\n'.join(text)
-                    # print(text)
-                    return text
-find_teacher('Панин.Ю.Н')
+        if type(name) is list:
+            if name[0].upper() == line_ziped[0].upper():
+                if name[1].upper() in line_ziped[1].upper():
+                    if name[2].upper() in line_ziped[2].upper():
+                        line = line.split(' : ')
+                        text = [
+                        f'<b>Имя</b>: {line[0]}',
+                        f'<b>Ссылка</b>: {line[1]}'
+                        ]
+                        text = '\n'.join(text)
+                        print(text)
+                        return text
+        else:
+            if name.upper() == line_ziped[0].upper():
+                line = line.split(' : ')
+                text = [
+                f'<b>Имя</b>: {line[0]}',
+                f'<b>Ссылка</b>: {line[1]}'
+                ]
+                text = '\n'.join(text)
+                print(text)
+                return text
+
+
+find_teacher('Панин')
